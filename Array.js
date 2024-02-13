aa33dbe49aa45bee5100e555b1a89a469f0e6d5c
Shimaeel <154746513+Shimaeel@users.noreply.github.com>
2024-02-13 15:52:20 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,93 @@
+// replace a number in array: Question - 4
+// let array = [1, 2, 3, 4, 5];
+
+
+// console.log(array);
+// document.write(array + "<br>")
+
+
+
+// // calculate the length of the array: Question - 10
+// let myArray1 = [1, 2, 3, 4, 5];
+
+// let arrayLength = myArray1.length; // Google reference
+
+// console.log(arrayLength);
+// document.write(arrayLength + "<br>")
+
+
+
+
+// // Reverse an array: Question - 9
+// let Array = [1, 2, 3, 4, 5];
+
+// Array.reverse(); // Google reference 
+
+// console.log(Array); 
+// document.write(Array + "<br>");
+
+
+
+
+// // How do you check if an element exist in an Array: Question - 5
+
+// let myArray2 = [1, 2, 3, 4, 5];
+// let elementToCheck = 3;
+
+// if (myArray2.includes(elementToCheck)) { // .include google reference 
+//     console.log("exists in the array.");
+// } else {
+//     console.log("does not exist in the array.");
+// }
+
+
+
+
+// // Insert an item into an array at specific index: Question - 1
+// let array2 = [1, 2, 3, 4, 5];
+
+// array2[3] = 20;
+// console.log(array2);
+// document.write(array2 + "<br>");
+
+
+
+// // Remove an element from an array in javaScript: Question - 2
+
+// let array3 = [1, 2, 3, 4, 5];
+
+// array3[3] = '';
+// console.log(array3);
+// document.write(array3 + "<br>");
+
+
+// // Form a new Array and remove all the duplicate elements in a Array: Question - 3 
+
+// // let array3 = [1, 2, 3, 4, 5];
+
+
+
+
+// // Swap any two numbers in Array: Question - 6
+ 
+// let array4 = [2, 4, 6, 8, 10]
+
+// array4[3] = 10;
+// array4[4] = 8;
+
+// console.log(array4);
+// document.write(array4 + "<br>");
+
+
+
+
+// // Sum of each and every element present in an array: Question - 7 --
+
+// let array5 = [5, 10, 15, 20, 25]
+// let sum = 0;
+
+//   for (let i = 0; i < array5.length; i++) {
+//     sum += array5[i];
+//   }
+//   document.write(sum);
+//   console.log(sum);
